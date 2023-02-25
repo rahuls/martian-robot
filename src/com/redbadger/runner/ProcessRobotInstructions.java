077685720aa7503aaa045682fae2cd9e80421a9a
Rahul Sudhanaboina <sudhanaboina.rahul@gmail.com>
2023-02-25 12:05:32 +0000

    Utilised list to map direction to int which replaces if else constructs

--- src/com/redbadger/runner/ProcessRobotInstructions.java
+++ src/com/redbadger/runner/ProcessRobotInstructions.java
@@ -1,25 +1,43 @@
 package com.redbadger.runner;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 
+/**
+ * @author rahulsudhanaboina
+ *
+ */
 public class ProcessRobotInstructions {
+	
+	/**
+	 * This list is used to map direction (char) to integer and vice versa.
+	 * Using this list has helped us to replace large if else ladders.
+	 */
+	private final static List<Character> directionsMap = new ArrayList<>(Arrays.asList('N', 'E', 'S', 'W'));
+	
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		
-		//Creat max coordinates by scanning the first line
+		//Creat max coordinates of grid by scanning the first line
 		String maxCoordinateString = scanner.nextLine().toUpperCase();
 		Point maxPoint = new Point(Character.getNumericValue(maxCoordinateString.charAt(0)), Character.getNumericValue(maxCoordinateString.charAt(1)));
 		
-		StringBuilder sb = new StringBuilder();
-		
+		//Process each robot instruction
 		while(scanner.hasNextLine()) {
 			String line = scanner.nextLine().toUpperCase();
 			if(line != "") {
+				
+				//Get point and direction of the robot
 				Point currentPoint = new Point(Character.getNumericValue(line.charAt(0)), Character.getNumericValue(line.charAt(1)));				
-				Direction direction = new Direction(line.charAt(2));
+				Direction direction = new Direction(directionsMap.indexOf(line.charAt(2)));
+				
+				//Get the instructions
 				String instructions = scanner.nextLine().toUpperCase();
 				
+				//process instructions of the robot with particular point and direction
 				moveRobotAsPerInstructions(currentPoint, direction, maxPoint, instructions);	
 			}
 		}
@@ -29,53 +47,53 @@ public class ProcessRobotInstructions {
 	private static void moveRobotAsPerInstructions(Point currentPoint, Direction direction, Point maxPoint,
 			String instructions) {
 		
+		//flag used to check whether robot is lost. If its lost, the flag is set as false and loop is terminated.
 		boolean flag = true;
+		
 		for (char instruction : instructions.toCharArray())
-			if(processInstruction(currentPoint, direction, maxPoint, instruction) == false) {
+			if(processEachInstruction(currentPoint, direction, maxPoint, instruction) == false) {
 				flag=false;
 				break;
 			}
 		
-		
-		System.out.println(currentPoint + " " + direction.getFacing() + ((flag == false)? " LOST": ""));
+		System.out.println(currentPoint.getX()+" "+currentPoint.getY() + " " + directionsMap.get(direction.getFacing()) + ((flag == false)? " LOST": ""));
 	}
-
-	private static boolean processInstruction(Point currentPoint, Direction direction, Point maxPoint, char instruction) {
+	
+	/**
+	 * @return true if the robot moves forward (location is updated), false if robot is fallen of the edge.
+	 */
+	private static boolean processEachInstruction(Point currentPoint, Direction direction, Point maxPoint, char instruction) {
 		if(instruction == 'L' || instruction == 'R' )
 			changeRobotFacing(direction, instruction);
 		else
 			return moveRobotForward(currentPoint, direction, maxPoint);
 		return true;
-			
 	}
 	
 	/**
-	 * @param currentPoint
-	 * @param direction
-	 * @param maxPoint
-	 * @return boolean false if the robot moves into an edge, true if robot moves into a valid position in grid
+	 * @return true if the robot moves forward (location is updated), false if robot is fallen of the edge.
 	 */
 	private static boolean moveRobotForward(Point currentPoint, Direction direction, Point maxPoint) {
-		char currentFacing = direction.getFacing();
-		if(currentFacing == 'N') {
+		int currentFacing = direction.getFacing();
+		if(currentFacing == 0) {
 			if(currentPoint.getY()+1>maxPoint.getY()) {
 				return false;
 			}
 			currentPoint.increment('y');
 		}
-		else if(currentFacing == 'E') {
+		else if(currentFacing == 1) {
 			if(currentPoint.getX()+1>maxPoint.getX()) {
 				return false;
 			}
 			currentPoint.increment('x');
 		}
-		else if(currentFacing == 'S') {
+		else if(currentFacing == 2) {
 			if(currentPoint.getY()-1<0) {
 				return false;
 			}
 			currentPoint.decrement('y');
 		}
-		else if(currentFacing == 'W') {
+		else if(currentFacing == 3) {
 			if(currentPoint.getX()-1<0) {
 				return false;
 			}
@@ -84,43 +102,24 @@ public class ProcessRobotInstructions {
 		return true;
 	}
 
+	/**
+	 * Changes facing of the robot, the method uses int value as facing of the robot. 
+	 * If the instruction is L, the decrement the facing value. If the instruction is R, increment the value and perform mod 4 on the new value. 
+	 */
 	private static void changeRobotFacing(Direction direction, char instruction) {
-		char currentFacing = direction.getFacing();
-		char newFacing = 'O';
+		
+		int currentFacing = direction.getFacing();
+		int newFacing = 0;
+		
 		if(instruction == 'L') {
-			switch (currentFacing) {
-			case 'N':
-				newFacing = 'W';
-				break;
-			case 'E':
-				newFacing = 'N';
-				break;
-			case 'S':
-				newFacing = 'E';
-				break;
-			case 'W':
-				newFacing = 'S';
-				break;
-
-			}
+			if(currentFacing==0)
+				newFacing=3;
+			else 
+				newFacing = currentFacing - 1;
 		}
-		else if(instruction == 'R') {
-			switch (currentFacing) {
-			case 'N':
-				newFacing = 'E';
-				break;
-			case 'E':
-				newFacing = 'S';
-				break;
-			case 'S':
-				newFacing = 'W';
-				break;
-			case 'W':
-				newFacing = 'N';
-				break;
-
-			}
-		}
+		else if(instruction == 'R') 
+			newFacing = (currentFacing+1) % 4;
+		
 		direction.setFacing(newFacing);
 	}
 }
@@ -167,17 +166,17 @@ class Point{
 }
 
 class Direction {
-	char facing;
+	int facing;
 	
-	public Direction(char direction) {
+	public Direction(int direction) {
 		this.facing = direction;
 	}
 	
-	public char getFacing() {
+	public int getFacing() {
 		return facing;
 	}
 	
-	public void setFacing(char facing) {
+	public void setFacing(int facing) {
 		this.facing = facing;
 	}
 }
